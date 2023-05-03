258a87540c2a553a33708690ff212deea8ef5899
lwang <lwang79@gmail.com>
2023-05-03 18:01:43 +1000

    Before UI design

---
+++ IntJob.DataAccess/Models/Enums.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace IntJob.DataAccess.Models
+{
+	enum JobType
+	{
+		BH = 1,
+		ABH,
+		SAT,
+		SUN,
+		PH
+	}
+
+	enum JobCategory
+	{
+		Telephone = 1,
+		OnSite,
+		Video,
+		Others
+	}
+
+	enum JobStatus
+	{
+		Booked = 1,
+		Canceled,
+		Completed
+	}
+}
+
--- IntJob.DataAccess/Models/JobModel.cs
+++ IntJob.DataAccess/Models/JobModel.cs
@@ -14,6 +14,57 @@ namespace IntJob.DataAccess.Models
 		public string Comment { get; set; } = "";
 		public int Status { get; set; }			// Enum
 		public int RateId { get; set; }			// Forin Key
+
+		public static float EstimateIncome(int duration, RateModel rate)
+		{
+			return duration <= rate.MinTime ? rate.MinTimeRate :
+				(float)Math.Round((double)((duration - rate.MinTime) / rate.EachTime))
+					* rate.EachTimeRate + rate.MinTimeRate;
+		}
+
+		public float CalculateIncome(RateModel rate)
+		{
+			float income = 0;
+
+			if (Duration <= rate.MinTime)
+			{
+				income = rate.MinTimeRate;
+			}
+			else if (Duration >= rate.DeductThreshold)
+			{
+				int duration = Duration - rate.DeductTime;
+				income = (float)Math.Round((double)((duration - rate.MinTime) / rate.EachTime))
+                    * rate.EachTimeRate + rate.MinTimeRate;
+            }
+			else
+			{
+                income = (float)Math.Round((double)((Duration - rate.MinTime) / rate.EachTime))
+                    * rate.EachTimeRate + rate.MinTimeRate;
+            }
+
+			if (Status == (int)JobStatus.Canceled && CancelAt != null)
+			{
+                DateTimeOffset startAt = DateTimeOffset.Parse(StartAt);
+                DateTimeOffset cancelAt = DateTimeOffset.Parse(CancelAt);
+                TimeSpan gap = startAt - cancelAt;
+                int gapInMinutes = (int)gap.TotalMinutes;
+
+				if (gapInMinutes < rate.LateCancelTime)
+				{
+					income *= rate.LateCancelRate;
+				} else if (gapInMinutes < rate.EarlyCancelTime)
+				{
+					income *= rate.EarlyCancelRate;
+				} else
+				{
+					income = 0;
+				}
+            }
+
+			Income = (float)Math.Round((double)income, 2);
+
+			return Income;
+		}
     }
 }
 
